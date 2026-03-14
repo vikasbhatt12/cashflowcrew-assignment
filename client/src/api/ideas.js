55881bb16be276ab601c8aa50487db6d1fb100de
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2026-03-15 00:34:50 +0530

    fix the url issue

--- client/src/api/ideas.js
+++ client/src/api/ideas.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ideaspark-backend.onrender.com'
+  baseURL: 'https://ideaspark-backend.onrender.com/api'
 });
 
 export const fetchIdeas = async (page = 1, limit = 10, search = '', sort = 'newest') => {
